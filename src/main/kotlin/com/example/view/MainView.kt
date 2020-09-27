8e9648c2a483ee38344380aa2e5fabe899650e1c
YoshiS214 <60101499+YoshiS214@users.noreply.github.com>
2020-09-27 21:17:44 +0100

    Make digital clock work

--- src/main/kotlin/com/example/view/MainView.kt
+++ src/main/kotlin/com/example/view/MainView.kt
@@ -20,48 +20,53 @@ class MainView : View("Desktop App for RasPi") {
     private val analogClock: GroupLayout.Group by fxid<GroupLayout.Group>("Analog_Clock")
 
     init {
-        val digitalClockThread : Thread = Thread(Runnable{Run_Digital_Clock()})
-        //service.execute(digitalClockThread)
-        digitalClockThread.start()
+        val digitalClockThread : Thread = Thread(Runnable{runDigitalClock()})
+        service.execute(digitalClockThread)
+        //digitalClockThread.start()
         setButton.action {
-            digitalClockThread.stop()
-            digitalClockThread.start()
+            //digitalClockThread.stop()
+            //digitalClockThread.start()
+            service.shutdownNow()
+            service.execute(digitalClockThread)
         }
     }
 
-    private fun Run_Digital_Clock(){
+    private fun runDigitalClock(){
         val hourDigit : Text = digitalClock.children[0] as Text
         val minuteDigit : Text = digitalClock.children[2] as Text
         val current : Triple<Int, Int, Int> = getTime()
-        var second : Int = current.third
-        var hour : Int = current.first
-        var minute : Int = current.second
-
-        do {
+        var second = current.third
+        var hour = current.first
+        var minute = current.second
+        try {
             do {
-                if (hour < 10){
-                    hourDigit.text = "0$hour"
-                } else{
-                    hourDigit.text = hour.toString()
-                }
                 do {
-                    if (minute < 10){
-                        minuteDigit.text = "0$minute"
-                    }else{
-                        minuteDigit.text = minute.toString()
+                    if (hour < 10) {
+                        hourDigit.text = "0$hour"
+                    } else {
+                        hourDigit.text = hour.toString()
                     }
                     do {
-                        Thread.sleep(1000)
-                        second++
-                    } while (second < 60)
-                    second = 0
-                    minute++
-                } while (minute < 60)
-                minute = 0
-                hour++
-            } while (hour < 24)
-            hour = 0
-        } while (true)
+                        if (minute < 10) {
+                            minuteDigit.text = "0$minute"
+                        } else {
+                            minuteDigit.text = minute.toString()
+                        }
+                        do {
+                            Thread.sleep(1000)
+                            second++
+                        } while (second < 60)
+                        second = 0
+                        minute++
+                    } while (minute < 60)
+                    minute = 0
+                    hour++
+                } while (hour < 24)
+                hour = 0
+            } while (true)
+        }catch(e : Exception){
+            throw RuntimeException(e);
+        }
     }
 
     private fun getTime() : Triple<Int, Int, Int> {
